d6cae65e5ef2dc38f6cf5fb309887d9a5d9c1716
David Park <daviidpaark@gmail.com>
2022-09-17 18:19:57 -0400

    Implement default zoom when selecting state(values stored in Map)

--- client/src/components/DistrictChart.js
+++ client/src/components/DistrictChart.js
@@ -7,6 +7,18 @@ import {
   ZoomableGroup,
 } from "react-simple-maps";
 
+const xy = new Map([
+  ["Maryland", [-77.35486519706453, 39.056005538826696]],
+  ["Mississippi", [-89.88180841231264, 32.67701591286358]],
+  ["Georgia", [-83.17549980649974, 32.79853181919429]],
+]);
+
+const z = new Map([
+  ["Maryland", 8],
+  ["Mississippi", 7],
+  ["Georgia", 8],
+]);
+
 const DistrictChart = ({ state, setSelection }) => {
   const geo = "/maps/" + state + "-districts.json";
 
@@ -16,7 +28,7 @@ const DistrictChart = ({ state, setSelection }) => {
         projection="geoAlbersUsa"
         style={{ width: "100%", height: "800px" }}
       >
-        <ZoomableGroup zoom={1} center={[0, 0]}>
+        <ZoomableGroup zoom={z.get(state)} center={xy.get(state)}>
           <Geographies geography={geo}>
             {({ geographies }) =>
               geographies.map((geo) => (
