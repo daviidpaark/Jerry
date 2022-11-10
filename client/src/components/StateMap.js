58fec9082a8357560da124b04f183aeade974bcf
David Park <daviidpaark@gmail.com>
2022-11-10 13:57:07 -0500

    Change line stroke color of maps

--- client/src/components/StateMap.js
+++ client/src/components/StateMap.js
@@ -1,149 +1,150 @@
 import { Tooltip, Typography } from "@mui/material";
 import React, { memo, useState, useEffect } from "react";
 import {
-  ComposableMap,
-  Geographies,
-  Geography,
-  ZoomableGroup,
+	ComposableMap,
+	Geographies,
+	Geography,
+	ZoomableGroup,
 } from "react-simple-maps";
 import MapControls from "./MapControls";
 
 const xy = new Map([
-  ["Maryland", [-77.35486519706453, 39.056005538826696]],
-  ["Mississippi", [-89.88180841231264, 32.67701591286358]],
-  ["Georgia", [-83.17549980649974, 32.79853181919429]],
+	["Maryland", [-77.35486519706453, 39.056005538826696]],
+	["Mississippi", [-89.88180841231264, 32.67701591286358]],
+	["Georgia", [-83.17549980649974, 32.79853181919429]],
 ]);
 
 const z = new Map([
-  ["Maryland", 8],
-  ["Mississippi", 7],
-  ["Georgia", 8],
+	["Maryland", 8],
+	["Mississippi", 7],
+	["Georgia", 8],
 ]);
 
 const POSTAL = new Map([
-  ["Georgia", "ga"],
-  ["Maryland", "md"],
-  ["Mississippi", "ms"],
+	["Georgia", "ga"],
+	["Maryland", "md"],
+	["Mississippi", "ms"],
 ]);
 
 const StateMap = ({
-  state,
-  district,
-  switchMap,
-  setMap,
-  setDistrict,
-  setState,
-  setSwitchMap,
+	state,
+	district,
+	switchMap,
+	setMap,
+	setDistrict,
+	setState,
+	setSwitchMap,
 }) => {
-  let [geo, fetchMap] = useState(null);
-  useEffect(() => {
-    const request = new Request("/api/maps/" + POSTAL.get(state), {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    fetch(request)
-      .then((response) => response.json())
-      .then((data) => fetchMap(data));
-  }, [state]);
+	let [geo, fetchMap] = useState(null);
+	useEffect(() => {
+		const request = new Request("/api/maps/" + POSTAL.get(state), {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		fetch(request)
+			.then((response) => response.json())
+			.then((data) => fetchMap(data));
+	}, [state]);
 
-  const [position, setPosition] = useState({
-    coordinates: xy.get(state),
-    zoom: z.get(state),
-  });
+	const [position, setPosition] = useState({
+		coordinates: xy.get(state),
+		zoom: z.get(state),
+	});
 
-  function handleZoomIn() {
-    if (position.zoom >= 12) return;
-    setPosition((pos) => ({ ...pos, zoom: pos.zoom * 1.5 }));
-  }
+	function handleZoomIn() {
+		if (position.zoom >= 12) return;
+		setPosition((pos) => ({ ...pos, zoom: pos.zoom * 1.5 }));
+	}
 
-  function handleZoomOut() {
-    if (position.zoom <= 1) return;
-    setPosition((pos) => ({ ...pos, zoom: pos.zoom / 1.5 }));
-  }
+	function handleZoomOut() {
+		if (position.zoom <= 1) return;
+		setPosition((pos) => ({ ...pos, zoom: pos.zoom / 1.5 }));
+	}
 
-  function handleMoveEnd(position) {
-    setPosition(position);
-  }
+	function handleMoveEnd(position) {
+		setPosition(position);
+	}
 
-  function handleReset() {
-    setPosition((position) => ({
-      ...position,
-      coordinates: xy.get(state),
-      zoom: z.get(state),
-    }));
-  }
+	function handleReset() {
+		setPosition((position) => ({
+			...position,
+			coordinates: xy.get(state),
+			zoom: z.get(state),
+		}));
+	}
 
-  return (
-    <div data-tip="">
-      <MapControls
-        exit={1}
-        setMap={setMap}
-        setDistrict={setDistrict}
-        setState={setState}
-        switchMap={switchMap}
-        setSwitchMap={setSwitchMap}
-        state={state}
-        handleZoomIn={handleZoomIn}
-        handleZoomOut={handleZoomOut}
-        handleReset={handleReset}
-      ></MapControls>
-      <ComposableMap
-        projection="geoAlbersUsa"
-        style={{ width: "100%", height: "800px" }}
-      >
-        <ZoomableGroup
-          zoom={position.zoom}
-          center={position.coordinates}
-          onMoveEnd={handleMoveEnd}
-        >
-          <Geographies geography={geo}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <Tooltip
-                  title={
-                    <Typography fontSize={20}>
-                      {geo.properties.DISTRICT}
-                    </Typography>
-                  }
-                  placement="top"
-                  arrow
-                  followCursor
-                  enterDelay={0}
-                  leaveDelay={0}
-                >
-                  <Geography
-                    key={geo.rsmKey}
-                    geography={geo}
-                    onClick={() => {
-                      if(switchMap && district!==geo.properties.DISTRICT) setDistrict(geo.properties.DISTRICT)
-                    }}
-                    stroke="#AAA"
-                    strokeWidth={0.1}
-                    style={{
-                      default: {
-                        fill:
-                          district === geo.properties.DISTRICT
-                            ? "#a5d6a7"
-                            : "#EEEEEE",
-                        outline: "none",
-                      },
-                      hover: {
-                        fill: "#ce93d8",
-                        cursor: "pointer",
-                        outline: "none",
-                      },
-                    }}
-                  />
-                </Tooltip>
-              ))
-            }
-          </Geographies>
-        </ZoomableGroup>
-      </ComposableMap>
-    </div>
-  );
+	return (
+		<div data-tip="">
+			<MapControls
+				exit={1}
+				setMap={setMap}
+				setDistrict={setDistrict}
+				setState={setState}
+				switchMap={switchMap}
+				setSwitchMap={setSwitchMap}
+				state={state}
+				handleZoomIn={handleZoomIn}
+				handleZoomOut={handleZoomOut}
+				handleReset={handleReset}
+			></MapControls>
+			<ComposableMap
+				projection="geoAlbersUsa"
+				style={{ width: "100%", height: "800px" }}
+			>
+				<ZoomableGroup
+					zoom={position.zoom}
+					center={position.coordinates}
+					onMoveEnd={handleMoveEnd}
+				>
+					<Geographies geography={geo}>
+						{({ geographies }) =>
+							geographies.map((geo) => (
+								<Tooltip
+									title={
+										<Typography fontSize={20}>
+											{geo.properties.DISTRICT}
+										</Typography>
+									}
+									placement="top"
+									arrow
+									followCursor
+									enterDelay={0}
+									leaveDelay={0}
+								>
+									<Geography
+										key={geo.rsmKey}
+										geography={geo}
+										onClick={() => {
+											if (switchMap && district !== geo.properties.DISTRICT)
+												setDistrict(geo.properties.DISTRICT);
+										}}
+										stroke="#000"
+										strokeWidth={0.1}
+										style={{
+											default: {
+												fill:
+													district === geo.properties.DISTRICT
+														? "#a5d6a7"
+														: "#EEEEEE",
+												outline: "none",
+											},
+											hover: {
+												fill: "#ce93d8",
+												cursor: "pointer",
+												outline: "none",
+											},
+										}}
+									/>
+								</Tooltip>
+							))
+						}
+					</Geographies>
+				</ZoomableGroup>
+			</ComposableMap>
+		</div>
+	);
 };
 
 export default memo(StateMap);
--- client/src/components/UsMap.js
+++ client/src/components/UsMap.js
@@ -1,135 +1,135 @@
 import { Tooltip, Typography } from "@mui/material";
 import React, { memo, useState, useEffect } from "react";
 import {
-  ComposableMap,
-  Geographies,
-  Geography,
-  ZoomableGroup,
+	ComposableMap,
+	Geographies,
+	Geography,
+	ZoomableGroup,
 } from "react-simple-maps";
 import MapControls from "./MapControls";
 
 const UsMap = ({ setState, setMap }) => {
-  const [geo, fetchMap] = useState(null);
-  useEffect(() => {
-    const request = new Request("/api/maps/us", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    fetch(request)
-      .then((response) => response.json())
-      .then((data) => fetchMap(data));
-  }, []);
+	const [geo, fetchMap] = useState(null);
+	useEffect(() => {
+		const request = new Request("/api/maps/us", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		fetch(request)
+			.then((response) => response.json())
+			.then((data) => fetchMap(data));
+	}, []);
 
-  const [position, setPosition] = useState({
-    coordinates: [-96.6, 38.7],
-    zoom: 1,
-  });
+	const [position, setPosition] = useState({
+		coordinates: [-96.6, 38.7],
+		zoom: 1,
+	});
 
-  function handleZoomIn() {
-    if (position.zoom >= 4) return;
-    setPosition((pos) => ({ ...pos, zoom: pos.zoom * 1.5 }));
-  }
+	function handleZoomIn() {
+		if (position.zoom >= 4) return;
+		setPosition((pos) => ({ ...pos, zoom: pos.zoom * 1.5 }));
+	}
 
-  function handleZoomOut() {
-    if (position.zoom <= 1) return;
-    setPosition((pos) => ({ ...pos, zoom: pos.zoom / 1.5 }));
-  }
+	function handleZoomOut() {
+		if (position.zoom <= 1) return;
+		setPosition((pos) => ({ ...pos, zoom: pos.zoom / 1.5 }));
+	}
 
-  function handleMoveEnd(position) {
-    setPosition(position);
-  }
+	function handleMoveEnd(position) {
+		setPosition(position);
+	}
 
-  function handleReset() {
-    setPosition((position) => ({
-      ...position,
-      coordinates: [-96.6, 38.7],
-      zoom: 1,
-    }));
-  }
+	function handleReset() {
+		setPosition((position) => ({
+			...position,
+			coordinates: [-96.6, 38.7],
+			zoom: 1,
+		}));
+	}
 
-  return (
-    <div data-tip="">
-      <MapControls
-        handleZoomIn={handleZoomIn}
-        handleZoomOut={handleZoomOut}
-        handleReset={handleReset}
-      ></MapControls>
-      <ComposableMap
-        projection="geoAlbersUsa"
-        style={{ width: "100%", height: "800px" }}
-      >
-        <ZoomableGroup
-          zoom={position.zoom}
-          center={position.coordinates}
-          onMoveEnd={handleMoveEnd}
-        >
-          <Geographies geography={geo}>
-            {({ geographies }) =>
-              geographies.map((geo) => (
-                <Tooltip
-                  title={
-                    <Typography fontSize={20}>{geo.properties.name}</Typography>
-                  }
-                  placement="top"
-                  arrow
-                  followCursor
-                >
-                  <Geography
-                    key={geo.rsmKey}
-                    geography={geo}
-                    stroke="#AAA"
-                    strokeWidth={0.5}
-                    onClick={() => {
-                      if (
-                        geo.properties.name === "Maryland" ||
-                        geo.properties.name === "Mississippi" ||
-                        geo.properties.name === "Georgia"
-                      ) {
-                        setState(geo.properties.name);
-                        setMap(true);
-                      }
-                    }}
-                    style={{
-                      default: {
-                        fill:
-                          geo.properties.name === "Maryland" ||
-                          geo.properties.name === "Mississippi" ||
-                          geo.properties.name === "Georgia"
-                            ? "#a5d6a7"
-                            : "#EEEEEE",
-                        outline: "none",
-                      },
-                      hover: {
-                        fill:
-                          geo.properties.name === "Maryland" ||
-                          geo.properties.name === "Mississippi" ||
-                          geo.properties.name === "Georgia"
-                            ? "#ce93d8"
-                            : "#D6D6DA",
-                        outline: "none",
-                        cursor: "pointer",
-                      },
-                      pressed: {
-                        fill:
-                          geo.properties.name === "Maryland" ||
-                          geo.properties.name === "Mississippi" ||
-                          geo.properties.name === "Georgia"
-                            ? "#E42"
-                            : "#D6D6DA",
-                        outline: "none",
-                      },
-                    }}
-                  />
-                </Tooltip>
-              ))
-            }
-          </Geographies>
-        </ZoomableGroup>
-      </ComposableMap>
-    </div>
-  );
+	return (
+		<div data-tip="">
+			<MapControls
+				handleZoomIn={handleZoomIn}
+				handleZoomOut={handleZoomOut}
+				handleReset={handleReset}
+			></MapControls>
+			<ComposableMap
+				projection="geoAlbersUsa"
+				style={{ width: "100%", height: "800px" }}
+			>
+				<ZoomableGroup
+					zoom={position.zoom}
+					center={position.coordinates}
+					onMoveEnd={handleMoveEnd}
+				>
+					<Geographies geography={geo}>
+						{({ geographies }) =>
+							geographies.map((geo) => (
+								<Tooltip
+									title={
+										<Typography fontSize={20}>{geo.properties.name}</Typography>
+									}
+									placement="top"
+									arrow
+									followCursor
+								>
+									<Geography
+										key={geo.rsmKey}
+										geography={geo}
+										stroke="#000"
+										strokeWidth={0.5}
+										onClick={() => {
+											if (
+												geo.properties.name === "Maryland" ||
+												geo.properties.name === "Mississippi" ||
+												geo.properties.name === "Georgia"
+											) {
+												setState(geo.properties.name);
+												setMap(true);
+											}
+										}}
+										style={{
+											default: {
+												fill:
+													geo.properties.name === "Maryland" ||
+													geo.properties.name === "Mississippi" ||
+													geo.properties.name === "Georgia"
+														? "#a5d6a7"
+														: "#EEEEEE",
+												outline: "none",
+											},
+											hover: {
+												fill:
+													geo.properties.name === "Maryland" ||
+													geo.properties.name === "Mississippi" ||
+													geo.properties.name === "Georgia"
+														? "#ce93d8"
+														: "#D6D6DA",
+												outline: "none",
+												cursor: "pointer",
+											},
+											pressed: {
+												fill:
+													geo.properties.name === "Maryland" ||
+													geo.properties.name === "Mississippi" ||
+													geo.properties.name === "Georgia"
+														? "#E42"
+														: "#D6D6DA",
+												outline: "none",
+											},
+										}}
+									/>
+								</Tooltip>
+							))
+						}
+					</Geographies>
+				</ZoomableGroup>
+			</ComposableMap>
+		</div>
+	);
 };
 
 export default memo(UsMap);
